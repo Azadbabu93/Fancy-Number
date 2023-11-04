da0a2893995888f00563d56776b6acf32139aea0
Azadbabu93 <149866202+Azadbabu93@users.noreply.github.com>
2023-11-04 16:11:33 +0530

    load
    first

---
+++ UserManagementAuthenticationApplication.java
@@ -0,0 +1,18 @@
+package com.fancyNumber.usermanagementauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/*Created By
+ * Dileep  Kumar Sharma @ 29/06/22 11:53 AM
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.fancyNumber"})
+public class UserManagementAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementAuthenticationApplication.class, args);
+    }
+
+}
