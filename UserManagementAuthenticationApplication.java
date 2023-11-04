45e83924128520961dd87bb9b9e24b0d19a3f68a
Azadbabu93 <149866202+Azadbabu93@users.noreply.github.com>
2023-11-04 16:12:37 +0530

    Update UserManagementAuthenticationApplication.java
    load

--- UserManagementAuthenticationApplication.java
+++ UserManagementAuthenticationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /*Created By
- * Dileep  Kumar Sharma @ 29/06/22 11:53 AM
+ * Azad kumar @ 29/06/22 11:53 AM
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.fancyNumber"})
